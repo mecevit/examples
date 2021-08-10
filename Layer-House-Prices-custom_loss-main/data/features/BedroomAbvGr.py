68c5753b0742529606fface78de057634b91adcb
Derrick Mwiti <mwitiderrick@gmail.com>
2021-08-10 15:02:48 +0300

    Delete Layer-House-Prices-custom_loss-main directory

--- Layer-House-Prices-custom_loss-main/data/features/BedroomAbvGr.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from typing import Any
-from layer import Dataset
-
-def build_feature(sdf: Dataset("house_prices_train")) -> Any:
-    df = sdf.to_pandas()
-    BedroomAbvGr = df[['BedroomAbvGr','Id']]
-    return BedroomAbvGr
